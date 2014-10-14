edbd97e10ed0bce78610d4d3811a37e439fc650f
cidro <pablo.canaleshernandez@gmail.com>
2014-10-13 22:16:56 -0400

    added local machine

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('JB-PABLOH02'),
+	'local' => array('JB-PABLOH02', 'pablo-laptop'),
 
 ));
 
